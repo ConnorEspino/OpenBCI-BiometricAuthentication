795408e22e3d3f98fc7d57b5bd2e0881c9f7cf83
Connor Espino <connorespino@u.boisestate.edu>
2023-12-02 17:13:46 -0700

    Updated SVM Classification to use Spectral Entropy

--- SVM-Classification.py
+++ SVM-Classification.py
@@ -2,45 +2,55 @@ import joblib
 import argparse
 import numpy as np
 from pathlib import Path
-from sklearn.svm import SVC
-from brainflow.data_filter import DataFilter
+from sklearn.metrics import accuracy_score
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('modelPath', type=str, help='The path to the .pkl file containing the model created from SVM-CreateModel.py')
-    parser.add_argument('testFile', type=str, help='The path to the Preprocessed EEG file to test for classification')
+    parser.add_argument('testData', type=str, help='The path to the directory containing labeled directories of preprocessed EEG data to test for classification')
     args = parser.parse_args()
 
     modelPath = Path(args.modelPath)
-    testFilePath = Path(args.testFile)
+    testPath = Path(args.testData)
 
     funData = joblib.load(modelPath)
-    smallestIMF = funData[0]
+    scaler = funData[0]
     imfRange = funData[1]
     model = funData[2]
     classDict = funData[3]
     reverse_dict = {value: key for key, value in classDict.items()}
 
-    imfs = np.load(str(testFilePath))
+    testData = []
+    classificationArray = []
 
-    # Only include IMFs that were included in the model
-    filteredFreqs = []
-    for i, imf in enumerate(imfs):
-        if (-1 in imfRange or i in imfRange):
-            filteredFreqs.append(imf[:smallestIMF])
+    for dir in testPath.iterdir():
+        if (dir.is_dir()):
+            for file in dir.iterdir():
+                data = np.load(str(file))
+                for i, entry in enumerate(data):
+                    if (-1 in imfRange or i in imfRange):
+                        SpecEntrReal = [np.real(entry), np.imag(entry)]
 
+                        testData.append(SpecEntrReal)
+                        classificationArray.append(classDict[dir.name])
+
+    testDataScaled = scaler.transform(testData)
     try:
-        arr = model.predict(filteredFreqs)
+        testPredictions = model.predict(testDataScaled)
+        # print(f'Test Data: {str(testDataScaled)}')
+        # print(f'Test Prediction: {str(testPredictions)}')
+        accuracy = accuracy_score(classificationArray, testPredictions)
+        print(f'Model Accuracy: {accuracy}')
     except Exception as e:
         print("\n\nError:\n" + str(e))
         return
 
-    finalGuesses = []
+    # finalGuesses = []
 
-    for elem in arr:
-        finalGuesses.append(reverse_dict[elem])
+    # for elem in arr:
+    #     finalGuesses.append(reverse_dict[elem])
 
-    print(finalGuesses)
+    # print(finalGuesses)
 
 
 if __name__ == "__main__":
